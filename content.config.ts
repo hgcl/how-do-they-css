798ebc8df1bf66fc1180bf9b69912c23232c3945
Clara Le <hey@clarale.com>
2026-01-21 17:12:26 +0100

    build(deps): set up nvm, eslint, prettier, @nuxt/content, @nuxt/fonts

---
+++ content.config.ts
@@ -0,0 +1,32 @@
+import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { z } from 'zod'
+
+const baseSchema = (config = {}) =>
+  z.object({
+    ...config,
+    css: z.string(),
+    cssHover: z
+      .object({
+        defaultBackgroundColor: z.string(),
+        hoverBackgroundColor: z.string()
+      })
+      .optional(),
+    source: z.string(),
+    comment: z.string(),
+    dateModified: z.string()
+  })
+
+export default defineContentConfig({
+  collections: {
+    buttons: defineCollection({
+      type: 'data',
+      source: 'buttons/**.json',
+      schema: baseSchema()
+    }),
+    shadows: defineCollection({
+      type: 'data',
+      source: 'shadows/**.json',
+      schema: baseSchema()
+    })
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/a11y"],
+  modules: ['@nuxt/eslint', '@nuxt/a11y', '@nuxt/content', '@nuxt/fonts'],
+  css: ['~/assets/globals.css'],
   eslint: {
     config: {
       // Format with ESLint
-      stylistic: true,
-    },
+      stylistic: true
+    }
   },
   a11y: {
     // Enable/disable the module (default: true in dev mode)
@@ -15,6 +16,6 @@ export default defineNuxtConfig({
     // Auto-highlight all violations when detected
     defaultHighlight: true,
     // Log violations to browser console
-    logIssues: true,
-  },
-});
+    logIssues: true
+  }
+})
