97f96d0b4fa39eb2b39254ede36c768a59f7faa4
Clara Le <hey@clarale.com>
2026-02-15 10:53:26 +0100

    docs: update site description

--- app/constants/seo.ts
+++ app/constants/seo.ts
@@ -1,3 +1,3 @@
 export const SITE_NAME = 'How do they CSS?'
 export const DEFAULT_DESCRIPTION =
-  'Copy and paste accessible, ready-to-use CSS code snippets from the best: buttons, box shadows, loading spinners, and more.'
+  'Copy and paste accessible, ready-to-use CSS code snippets from big brands: buttons, box shadows, loading spinners, and more.'
